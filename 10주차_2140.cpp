bd0e87378284765f9cd978c27ac7336b09b4a3ac
Haebin <hbyeon@student.42seoul.kr>
2023-02-02 00:52:49 +0900

    2023/02/02 00:52

--- 10주차_2140.cpp
+++ 10주차_2140.cpp
@@ -1,67 +1,79 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 #define ios_sync ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 
 char board[105][105];
+int dx[8] = {1, 1, 1, 0, 0, -1, -1, -1};
+int dy[8] = {1, 0, -1, 1, -1, -1, 0, -1};
 int n;
 
-void check_utr() {
-    for (int i = 1; i <= n; i++) {
-        if (board[i][1] == '0') { // check LeftUp to Right
-            if (board[i - 1][2] == '#') board[i - 1][2] = '1';
-            if (board[i][2] == '#') board[i][2] = '2';
-            if (board[i + 1][2] == '#') board[i - 1][2] = '3';
+void search_minefield(int x, int y){
+    int mine = 0;
+    int sharp = 0;
+    queue <pair<int, int> > sharp_idx;
+
+    for(int i = 0; i < 8; i++)
+    {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx < 1 || nx > n || ny < 1 || ny > n) continue;
+
+        if (board[nx][ny] == '#'){
+            sharp_idx.push(make_pair(nx, ny));
+            sharp++;
+        }
+        if (board[nx][ny] == '+')
+            mine++;
+    }
+    if (board[x][y] == mine) // 찾은 지뢰의 개수랑 보드의 지뢰 개수가 같아서, 이미 모든 지뢰를 찾았을 때
+                             // 여기는 뒤져도 지뢰가 아닙니다.
+    {
+        while(!sharp_idx.empty())
+        {
+            int temp1 = sharp_idx.front().first;
+            int temp2 = sharp_idx.front().second;
+            board[temp1][temp2] = '-';
+            sharp_idx.pop();
         }
     }
-}
-
-void check_ltd() {
-    for (int i = 1; i <= n; i++) {
-        if (board[1][i] == '0') { // check LeftUp to Down
-            if (board[2][i - 1] == '#') board[2][i - 1] = '4';
-            if (board[2][i] == '#') board[2][i] = '5';
-            if (board[2][i + 1] == '#') board[2][i + 1] = '6';
+    else if ((board[x][y]-48) == (mine + sharp))
+    {
+        while (!sharp_idx.empty())
+        {
+            int temp1 = sharp_idx.front().first;
+            int temp2 = sharp_idx.front().second;
+            board[temp1][temp2] = '+';
+            sharp_idx.pop();
         }
     }
+
 }
 
-void check_dtr() {
-    for (int i = 1; i <= n; i++) {
-        if (board[i][n] == '0') { // check LeftDown to Right
-            if (board[i - 1][n - 1] == '#') board[i - 1][n - 1] = '7';
-            if (board[i][n - 1] == '#') board[i - 1][n - 1] = '8';
-            if (board[i + 1][n - 1] == '#') board[i - 1][n - 1] = '9';
-        }
-    }
-}
-
-void check_rtd() {
-    for (int i = 1; i <= n; i++) {
-        if (board[n][i] == '0') { // check RightUp to Down
-            if (board[n - 1][i - 1] == '#') board[i - 1][n - 1] = 'a';
-            if (board[n - 1][i] == '#') board[i - 1][n - 1] = 'b';
-            if (board[n - 1][i + 1] == '#') board[i - 1][n - 1] = 'c';
-        }
-    }
-}
-
-int main()
-{
+int main() {
     ios_sync;
     cin >> n;
 
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= n; j++)
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
             cin >> board[i][j];
 
-    check_dtr();
-    check_ltd();
-    check_rtd();
-    check_utr();
 
-    int cnt = 0;
-    for(int i = 1; i <= n; i++)
-        for(int j = 1; j <= n; j++)
-            if(board[i][j] == '#') cnt++;
-    cout << cnt;
+    for (int x = 1; x <= n; x++)
+        search_minefield(x, 1);
+
+    for (int y = 1; y <= n; y++)
+        search_minefield(1, y);
+
+    for (int x = 1; x <= n; x++)
+        search_minefield(x, n);
+
+    for (int y = 1; y <= n; y++)
+        search_minefield(n, y);
+
+    for (int i = 1; i <= n; i++) {
+        cout << endl;
+        for (int j = 1; j <= n; j++)
+            cout << board[i][j];
+    }
 }
