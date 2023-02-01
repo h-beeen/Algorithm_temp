dab0451010f8772e9372b8baae77541431db1698
Haebin <hbyeon@student.42seoul.kr>
2023-02-02 00:53:16 +0900

    2023/02/02 00:52

---
+++ 9주차_26142.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+#define ios_sync ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+
+int n, a, b;
+vector <pair <int, int> > dr;
+
+int main(void)
+{
+    cin >> n;
+    for (int day = 1; day <= n; day++)
+    {
+        cin >> a >> b
+
+    }
+}
